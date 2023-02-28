336508fb550298a56e175a4ff644b6bb3ae39c8d
Ross Cohen <rosscohen1990@gmail.com>
2023-02-28 18:40:45 -0500

    new

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: '6e0caa523cc2fd7a4503bc85382a59cc',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
